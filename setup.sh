242ae07be9c15882de24bb0689b6a14d67b4a234
shifumin <shifumin4230@gmail.com>
2015-08-14 12:02:06 +0900

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOT_FILES=( .gemrc .gitconfig .gitignore .rubocop.yml .tmux.conf .vimrc .zshrc .zshrc.custom )
+
+
+# DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc .percol.d)
+
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+# install oh-my-zsh
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# install NeoBundle
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
+
