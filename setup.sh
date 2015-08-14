b982b470f0b69b2e529dc9bd9a390cdcffac5991
shifumin <shifumin4230@gmail.com>
2015-08-14 17:28:27 +0900

    Update target DOT_FILES

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 
 DOT_FILES=( .gemrc .gitconfig .gitignore .rubocop.yml .tmux.conf .vimrc .zshrc .zshrc.custom )
 
-
-# DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc .percol.d)
-
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
