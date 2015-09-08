646ef76883d28b74b5dac6b9cbe81e52b82d3467
shifumin <shifumin4230@gmail.com>
2015-09-08 17:17:26 +0900

    add 'install bin for tmux'

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# install dotfiles
 DOT_FILES=( .ansible.cfg .gemrc .gitconfig .gitignore .rubocop.yml .tmux.conf .vimrc .zshrc .zshrc.custom )
-
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+# install bin for tmux
+BIN_FILES=( battery )
+for file in ${BIN_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file /usr/local/bin/$file
 done
 
 # install oh-my-zsh
