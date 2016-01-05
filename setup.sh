eda47fc55e3d3dde97ae0c4f56fce4aa93950d0d
shifumin <shifumin4230@gmail.com>
2016-01-05 12:23:02 +0900

    add 'change shell for Zsh' and so on

--- setup.sh
+++ setup.sh
@@ -23,6 +23,12 @@ ln -s $HOME/dotfiles/php.vim $HOME/.vim/indent/php.vim
 # install oh-my-zsh
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+# change shell for Zsh
+chsh -s /bin/zsh
+
+# create a symbolic link for diff-hightlight
+ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin
+
 # install NeoBundle
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
 
