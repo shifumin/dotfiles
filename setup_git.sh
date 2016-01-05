4fe061d61f1d886bb746fcb36ff879479d1db2be
shifumin <shifumin4230@gmail.com>
2016-01-05 15:45:40 +0900

    fix minor things

--- setup_git.sh
+++ setup_git.sh
@@ -3,12 +3,12 @@
 sudo yum -y remove git
 sudo yum install -y gcc curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
 
-sudo cd /usr/local/src
+cd /usr/local/src
 sudo wget https://www.kernel.org/pub/software/scm/git/git-2.7.0.tar.gz
 sudo tar zxvf git-2.7.0.tar.gz
 
-sudo cd git-2.7.0/
+cd /usr/local/src/git-2.7.0
 sudo ./configure --prefix=/usr/local/
-sudo make $$ make install
+sudo make && sudo make install
 
-sudo ln -s /usr/local/src/git-2.7.0/contrib/diff-highlight/diff-highlight /usr/lolca/bin/diff-highlight
+sudo ln -s /usr/local/src/git-2.7.0/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
