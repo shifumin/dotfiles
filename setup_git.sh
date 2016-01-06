2a435df6449331ad9dd5d34b2cd963b652a66874
shifumin <shifumin4230@gmail.com>
2016-01-06 15:11:15 +0900

    minor update

--- setup_git.sh
+++ setup_git.sh
@@ -6,11 +6,11 @@ sudo yum -y remove git
 sudo yum install -y gcc curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
 
 cd /usr/local/src
-[ ! -e /usr/local/src/git-$GIT_VER.tar.gz ] && sudo wget https://www.kernel.org/pub/software/scm/git/git-$GIT_VER.tar.gz
-[ ! -e /usr/local/src/git-$GIT_VER ] && sudo tar zxvf git-$GIT_VER.tar.gz
+[ ! -e /usr/local/src/git-${GIT_VER}.tar.gz ] && sudo wget https://www.kernel.org/pub/software/scm/git/git-${GIT_VER}.tar.gz
+[ ! -e /usr/local/src/git-${GIT_VER} ] && sudo tar zxvf git-${GIT_VER}.tar.gz
 
-cd /usr/local/src/git-$GIT_VER
+cd /usr/local/src/git-${GIT_VER}
 [ ! -e /usr/local/bin/git ] && sudo ./configure --prefix=/usr/local/
 [ ! -e /usr/local/bin/git ] && sudo make && sudo make install
 
-sudo ln -s /usr/local/src/git-$GIT_VER/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+sudo ln -s /usr/local/src/git-${GIT_VER}/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
--- setup_zsh.sh
+++ setup_zsh.sh
@@ -5,10 +5,10 @@ ZSH_VER="5.2"
 sudo yum install -y ncurses-devel
 
 cd /usr/local/src
-[ ! -e /usr/local/src/zsh-$ZSH_VER.tar.gz ] && sudo wget http://downloads.sourceforge.net/project/zsh/zsh/$ZSH_VER/zsh-$ZSH_VER.tar.gz
-[ ! -e /usr/local/src/zsh-$ZSH_VER ] && sudo tar zxvf zsh-$ZSH_VER.tar.gz
+[ ! -e /usr/local/src/zsh-${ZSH_VER}.tar.gz ] && sudo wget http://downloads.sourceforge.net/project/zsh/zsh/${ZSH_VER}/zsh-${ZSH_VER}.tar.gz
+[ ! -e /usr/local/src/zsh-${ZSH_VER} ] && sudo tar zxvf zsh-${ZSH_VER}.tar.gz
 
-cd /usr/local/src/zsh-$ZSH_VER
+cd /usr/local/src/zsh-${ZSH_VER}
 [ ! -e /usr/local/bin/zsh ] && sudo ./configure
 [ ! -e /usr/local/bin/zsh ] && sudo make && sudo make install
 
