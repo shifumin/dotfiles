e421730e12ed9c8be3629cd33d87656268a0b3ea
shifumin <shifumin4230@gmail.com>
2016-01-07 14:39:29 +0900

    add create a symbolic link for peco

--- setup_peco.sh
+++ setup_peco.sh
@@ -7,5 +7,4 @@ cd /usr/local/src
 [ ! -e /usr/local/src/peco_linux_amd64.tar.gz ] && sudo wget https://github.com/peco/peco/releases/download/v${PECO_VER}/peco_linux_amd64.tar.gz
 [ ! -e /usr/local/src/peco_linux_amd64 ] && sudo tar zxvf peco_linux_amd64.tar.gz
 
-cd /usr/local/src/peco_linux_amd64
-sudo cp peco /usr/local/bin/
+sudo ln -s /usr/local/src/peco_linux_amd64/peco /usr/local/bin
