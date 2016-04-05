adbfe9f47bd3ab9cddf8d5e236b5cf19cb7ac8c4
shifumin <shifumin4230@gmail.com>
2016-04-05 18:36:44 +0900

    change ./configure ruby path option

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 sudo yum remove -y vim-common vim-enhanced
-sudo yum install -y mercurial lua-devel ncurses-devel
+sudo yum install -y mercurial lua-devel ncurses-devel readline-devel
+
+git clone git@github.com:sstephenson/rbenv.git ~/.rbenv
+git clone git@github.com:sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install 2.3.0
+rbenv rehash
+rbenv global 2.3.0
 
 cd /usr/local/src
 sudo hg clone https://vim.googlecode.com/hg vim
@@ -11,8 +17,8 @@ sudo hg update
 sudo make distclean
 
 # --prefix=/usr/local でもいいかも
-# --with-ruby-command=/home/vagrant/.rbenv/shims/ruby RubyのPATH
-sudo ./configure --prefix=/opt/vim  --with-features=huge --enable-multibyte --enable-rubyinterp --enable-luainterp --enable-cscope --enable-fail-if-missing --with-ruby-command=/usr/local/rbenv/shims/ruby --with-lua-prefix=/usr
+# --with-ruby-command=~/.rbenv/shims/ruby RubyのPATH
+sudo ./configure --prefix=/opt/vim  --with-features=huge --enable-multibyte --enable-rubyinterp --enable-luainterp --enable-cscope --enable-fail-if-missing --with-ruby-command=~/.rbenv/shims/ruby --with-lua-prefix=/usr
 sudo make
 sudo make install
 
